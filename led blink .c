4c8127f38329dd1294849b4af315c3c8ff27319e
shashi745 <68228933+shashi745@users.noreply.github.com>
2020-08-07 01:07:49 +0530

    Update led blink .c

--- led blink .c
+++ led blink .c
@@ -3,13 +3,13 @@
 unsigned int delay;
 int main(void)
 {
-  IO0DIR = (1<<10);   // declare  P0.09 as Output
+  IO0DIR = (1<<10);  
   
   while(1)  // forever loop 
   {
     IO0CLR = (1<<10);    //  clear to turn led on 
-    for(delay=0;delay<; delay++);
+    for(delay=0;delay<1000; delay++);
     IO0SET = (1<<10);    //   set to Turn Led on 
-    for(delay=0; delay<500000; delay++); 
+    for(delay=0; delay<1000; delay++); 
   }
 }
