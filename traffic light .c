ffb8162d1d68d2ec57017b0fb5fdd8153b0eda8b
shashi745 <68228933+shashi745@users.noreply.github.com>
2020-08-07 01:10:43 +0530

    Update traffic light .c

--- traffic light .c
+++ traffic light .c
@@ -1,30 +1,35 @@
-#include<lpc214x.h>
-#include<stdint.h>
 
-void delay_ms(uint16_t j)
+	
+#include<lpc21xx.h>    ////importing LPC2148 library
+#include<stdint> 
+void delay(uint16_t j) //function for delay for 12MHz clockspeed
 {
 	uint16_t x,i;
 	for(i=0;i<j;++i)
 	{
-		for(x=0;x<6000;x++);
+		for(x=0;x<60000;x++);
 	}
 	
 }
 int main(void)
 {
-	IO0DIR=0x000000FF;
+	
 	while(1)
 	{
-		IO0PIN=IO0PIN | 0x00000001;
-		delay_ms(300);
-		IO0PIN=IO0PIN & 0xFFFFFF00;
-		//delay_ms(300);
-		IO0PIN=IO0PIN | 0x00000002;
-		delay_ms(300);
-		IO0PIN=IO0PIN & 0xFFFFFF00;
-		//delay_ms(300);
-		IO0PIN=IO0PIN | 0x00000004;
-		delay_ms(300);
-		IO0PIN=IO0PIN & 0xFFFFFF00;
+		IO0DIR=(1<<10); //initializing pin 10
+		IO0SET = (1<<10);//setting pin 10 to high
+		delay(500);       //wait for 500ms
+		IO0CLR = (1<<10); //setting pin 10 to low
+		delay(500);        //wait for 500ms
+		IO0DIR=(1<<11);//initializing pin 11
+		IO0SET = (1<<11);//setting pin 11 to high
+		delay(500);       //wait for 500ms
+		IO0CLR = (1<<11); //setting pin 11 to low
+		delay(500);        //wait for 500ms
+		IO0DIR=(1<<12);//initializing pin 12
+		IO0SET = (1<<12);//setting pin 12 to high
+		delay(500);       //wait for 500ms
+		IO0CLR = (1<<12); //setting pin 12 to low
+		delay(500);       //wait for 500ms
 	}
 }
